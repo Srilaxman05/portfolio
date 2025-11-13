fe5cc041df84e9079dee5fe79b69cbafee157c46
Srilaxman05 <srilaxman05@gmail.com>
2025-11-13 13:47:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Portfolio Loaded Successfully!");
+// Simple form handler (you can connect this to a backend later)
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+  this.reset();
+});
